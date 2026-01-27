d421505e97fd31b5a95ed8261b3d86500c3158c1
Danniel Bruno <danniel@email.com>
2026-01-27 13:03:43 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import './questao.dart';
+
+main (){
+  runApp( MeuApp());
+
+}
+var _perguntaselecionada = 0;
+
+
+
+void _responder() {
+  _perguntaselecionada++;
+  print(_perguntaselecionada);
+}
+class MeuApp extends StatefulWidget {
+   MeuApp({super.key});
+
+  @override
+  State<MeuApp> createState() => _MeuAppState();
+}
+
+class _MeuAppState extends State<MeuApp> {
+  final perguntas =[
+  'qual sua cor favorita',
+  'qual seu anunmal favroito',
+];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(home:
+    Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: Text('PERGUNTAS'),
+      ),
+      body: Column(
+        children: [
+          Text(perguntas[_perguntaselecionada]),
+          ElevatedButton(
+child: Text('pergunta 1'),
+            onPressed: () {
+              _responder();
+            },
+
+          ),
+ElevatedButton(
+child: Text('pergunta 1'),
+onPressed: () {
+_responder();
+},
+
+),
+ElevatedButton(
+child: Text('pergunta 1'),
+onPressed: () {
+_responder();
+},
+
+),
+
+        ],
+      ),
+
+
+    ),
+    );
+  }
+}
